6ac9525c6e4149cd5a5e6d580f756dfaeb715aae
wadeen <meister.vvdo@gmail.com>
2022-12-11 22:29:44 +0900

    名前、メール、内容の追加＋サンクスページ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
+import style from "../styles/Home.module.css";
 
 export default function Home() {
   const [inputName, setInputName] = useState("");
-
-  const changeInputName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputName(e.target.value);
-  };
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputContents, setInputContents] = useState("");
 
   return (
-    <form method="post" action={process.env.NEWT_FORM_ENDPOINT}>
-      <input type="text" name="userName" value={inputName} onChange={changeInputName} />
-      <button type="submit">送信</button>
+    <form method="post" action={process.env.NEWT_FORM_ENDPOINT} className={style.container}>
+      <dl className={style.list}>
+        <div className={style.listRow}>
+          <dt className={style.term}>名前：</dt>
+          <dd className={style.desc}>
+            <input type="text" name="name" required onChange={(e) => setInputName(e.target.value)} value={inputName} />
+          </dd>
+        </div>
+        <div className={style.listRow}>
+          <dt className={style.term}>メールアドレス：</dt>
+          <dd className={style.desc}>
+            <input type="email" name="email" onChange={(e) => setInputEmail(e.target.value)} value={inputEmail} />
+          </dd>
+        </div>
+        <div className={style.listRow}>
+          <dt className={style.term}>内容：</dt>
+          <dd className={style.desc}>
+            <textarea name="body" onChange={(e) => setInputContents(e.target.value)}>
+              {inputContents}
+            </textarea>
+          </dd>
+        </div>
+      </dl>
+      <button type="submit" className={style.submitBtn}>
+        送信する
+      </button>
     </form>
   );
 }
