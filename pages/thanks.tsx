e72ab746f283946b2516d69a50ea137a880dac4c
wadeen <meister.vvdo@gmail.com>
2022-12-11 19:24:50 +0900

    基本機能実装

---
+++ pages/thanks.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Thanks = () => {
+  return (
+    <div>
+      お問い合わせありがとうございました。
+      <Link href={"/"}>お問い合わせページへ戻る</Link>
+    </div>
+  );
+};
+
+export default Thanks;
